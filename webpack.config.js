8b9c3123841ef757b190c452ab414db667c936c0
101piano <771594535@qq.com>
2017-05-15 11:40:17 +0800

    const webpack=require(webpack);

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const webpack=require('webpack');
+const path=require('path');
+module.exports={
+  entry: './js/src/index.js',
+  output: {
+    path: path.resolve(__dirname,'./js/bin'),
+    filename: 'index.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test:/\.css$/,    
+        use: [
+          {loader:'style-loader'},
+          {loader:'css-loader'}
+        ]
+      },
+    ]
+  },
+  plugins:[
+  ]
+
+ 
+}
